efd1c75b8a14cf72e237f960c152398076842308
lzz <344168752@qq.com>
2016-12-26 16:45:08 +0800

    GateSvr

---
+++ GateSvr.cpp
@@ -0,0 +1,56 @@
+#include "GateSvr.h"
+
+#include <muduo/base/Logging.h>
+
+using namespace muduo;
+using namespace muduo::net;
+
+GateServer::GateServer(EventLoop* loop,
+                       const InetAddress& listenAddr,
+                       int maxConnections)
+  : server_(loop, listenAddr, "GateSvr"),
+    numConnected_(0),
+    kMaxConnections_(maxConnections)
+{
+  server_.setConnectionCallback(
+      std::bind(&GateServer::onConnection, this, std::placeholders::_1));
+  server_.setMessageCallback(
+      std::bind(&GateServer::onMessage, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+}
+
+void GateServer::start()
+{
+  server_.start();
+}
+
+void GateServer::onConnection(const TcpConnectionPtr& conn)
+{
+  LOG_INFO << "GateSvr - " << conn->peerAddress().toIpPort() << " -> "
+           << conn->localAddress().toIpPort() << " is "
+           << (conn->connected() ? "UP" : "DOWN");
+
+  if (conn->connected())
+  {
+    ++numConnected_;
+    if (numConnected_ > kMaxConnections_)
+    {
+      conn->shutdown();
+      conn->forceCloseWithDelay(3.0);  // > round trip of the whole Internet.
+    }
+  }
+  else
+  {
+    --numConnected_;
+  }
+  LOG_INFO << "numConnected = " << numConnected_;
+}
+
+void GateServer::onMessage(const TcpConnectionPtr& conn,
+                           Buffer* buf,
+                           Timestamp time)
+{
+  string msg(buf->retrieveAllAsString());
+  LOG_INFO << conn->name() << " echo " << msg.size() << " bytes at " << time.toString();
+  conn->send(msg);
+}
+
