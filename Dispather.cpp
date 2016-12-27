5a5c0eb5ad5505b6c14bf67daabb2bed38e08b36
lzz <344168752@qq.com>
2016-12-27 15:51:34 +0800

    dispather

---
+++ Dispather.cpp
@@ -0,0 +1,1 @@
+#include <muduo/base/Logging.h>
---
+++ Dispather.h
@@ -0,0 +1,25 @@
+#ifndef __DISPATHER_H__
+#define __DISPATHER_H__
+
+class CallBack
+{
+public:
+
+private:
+
+};
+
+class Dispather
+{
+public:
+    Dispather();
+    ~Dispather();
+
+    //void onMessage(uint32_t cmd, std::function());
+
+private:
+
+};
+
+
+#endif
