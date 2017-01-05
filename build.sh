e320cd821fa1e9fd07d98f2e002870d8be6d78a4
lzz <344168752@qq.com>
2017-01-05 17:01:28 +0800

    proxy

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-g++ -std=c++11 main.cpp GateSvr.cpp Dispather.cpp BgMessageQueue.cpp Worker.cpp -g -L/usr/local/bin -lmuduo -lpthread -I/home/godman/webpoj/majiang/muduo-master/ -o svr
